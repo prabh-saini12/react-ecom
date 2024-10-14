b377e65e0f7f2fc77fbed190133b37b16c607dab
khushiram saini <prabhakarsaini800@gmail.com>
2024-10-14 11:56:41 +0530

    Refactor ProductCard component to add a button for filtering top rated products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,30 @@
 import Product from "./Product";
-import {products} from '../utils/constant'
+import { products } from "../utils/constant";
+import { useState } from "react";
 
 const ProductCard = () => {
-
+  const [listOfProduct, setListOfProduct] = useState(products);
 
   return (
-    <div className="product-card">
-     {
-        products.map((product,index)=>{
-            return <Product key={index} productlist={product}/>
-        })
-     }
+    <div>
+      <button
+        className="button"
+        onClick={() => {
+          console.log("Before", listOfProduct);
+          const filterProduct = listOfProduct.filter(
+            (product) => product.rating.rate >= 4
+          );
+          console.log("after", filterProduct);
+          setListOfProduct(filterProduct);
+        }}
+      >
+        Top Rated Product
+      </button>
+      <div className="product-card">
+        {listOfProduct.map((product, index) => {
+          return <Product key={index} productlist={product} />;
+        })}
+      </div>
     </div>
   );
 };
