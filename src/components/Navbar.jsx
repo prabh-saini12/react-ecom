af37c78ee3b6d3b14cf3fe9bba857fa514ffc7db
khushiram saini <prabhakarsaini800@gmail.com>
2024-10-15 22:46:47 +0530

    Refactor Navbar and ProductCard components to implement filtering functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 const Navbar = () => {
+  const [btnName, setBtnName] = useState("light");
+
   return (
     <div className="navbar">
       <h1>Logo</h1>
@@ -8,6 +11,16 @@ const Navbar = () => {
         <li>women</li>
         <li>kids</li>
         <li>cart</li>
+        <li>
+          <button
+            onClick={() => {
+              const type = btnName === "light" ? "dark" : "light";
+              setBtnName(type);
+            }}
+          >
+            {btnName}
+          </button>
+        </li>
       </ul>
     </div>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,27 @@
 import Product from "./Product";
-import { products } from "../utils/constant";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProductCard = () => {
-  const [listOfProduct, setListOfProduct] = useState(products);
+  const [listOfProduct, setListOfProduct] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const json = await response.json();
+    setListOfProduct(json);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
       <button
         className="button"
         onClick={() => {
-          console.log("Before", listOfProduct);
           const filterProduct = listOfProduct.filter(
             (product) => product.rating.rate >= 4
           );
-          console.log("after", filterProduct);
           setListOfProduct(filterProduct);
         }}
       >
